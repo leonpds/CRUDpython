78df10b9f268aadd3edee6362a4a913d72ce65c8
JoaoPtavare <leonar.pds@gmail.com>
2022-09-26 19:12:26 -0300

    menu interativo

---
+++ main.py
@@ -0,0 +1,18 @@
+def menu_principal():
+    print('**####### MENU ######**')
+    print('* [1] CADASTRAR ')
+    print('* [2] LISTAR ')
+    print('* [3] REMOVER ')
+    print('* [4] ATUALIZAR ')
+    print('* [5] FECHAR ')
+    print('########################')
+
+menu_principal()
+
+Escolha_opcao = input("Escolha a opção desejada: ")
+
+if Escolha_opcao == "1":
+
+
+
+
